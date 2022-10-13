2b12481d6f0805d353078d37dba8c9da32878cca
Lezgig <gaillegue.eliot@gmail.com>
2022-10-13 12:59:16 +0100

    added link and some css animations

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -7,14 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  loopArray: Array<string> = ["github_logo.png" ,"oracle_vm_logo.png","slack_logo.png", "jira_logo.png","docker_logo.png"] ;
+  loopArrayLogo: Array<string> = ["github_logo.png" ,"oracle_vm_logo.png","slack_logo.png", "jira_logo.png","docker_logo.png"] ;
+  loopArrayLink: Array<string> = ["https://github.com/Lezgig/weKraft","https://www.oracle.com/fr/virtualization/technologies/vm/downloads/virtualbox-downloads.html","https://slack.com/intl/fr-fr/","https://www.atlassian.com/fr/software/jira","https://hub.docker.com/r/bitnami/symfony/"]
+  loopArray: {logo: string, link: string}[] = [];
+  show: boolean = false;
 
   constructor() {
 
   }
 
-  ngOnInit(): void {
+  public getLogo(index: number){
 
+    return this.loopArray[index].logo;
+  }
+
+  public getLink(index: number){
+
+    return this.loopArray[index].link;
+
+  }
+
+  private populate(){
+    let i = 0;
+    this.loopArrayLogo.forEach(element => {
+      this.loopArray.push({logo:this.loopArrayLogo[i] , link: this.loopArrayLink[i]})
+      i++;
+    });
+  }
+
+  ngOnInit(): void {
+    this.populate();console.log(this.loopArray)
+
+    this.show = true;
   }
 
 }
